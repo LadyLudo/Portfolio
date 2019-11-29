12d71a84b36bb35640c143d16fe2823a38d97c95
Catherine Davis <catherinevtdavis@gmail.com>
2019-11-29 12:18:21 -0700

    created menu button functionality

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    $("#menu").toggleClass("hidden")
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    $("#menu").toggleClass("hidden")
 }
